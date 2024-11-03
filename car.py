ea433bea7bb6831be13b020b254f4b01bf9da6a2
Ayaan-Singh <asthana.divya@gmail.com>
2024-11-03 11:59:22 +0530

    hi

---
+++ car.py
@@ -0,0 +1,10 @@
+num1 = int(input("enter a number 1 for car 2 for bike"))
+
+if (num1 == 1):
+    print ("you have chosen car ")
+    num2 = int(input("enter a number 1 for sedan 2 for SUV"))
+    print ("great choice")
+elif (num1 == 2 ):
+    print ("you have chosen bike")
+    num3 = int(input("enter a number 1 for bike 2 for motorcycle "))
+    print ("great choice")
